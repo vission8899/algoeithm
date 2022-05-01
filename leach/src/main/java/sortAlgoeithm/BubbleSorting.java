45f21083dcf379ce82e85f01554603585dbc048b
刘琦 <1014680094@qq.com>
2022-05-01 15:27:21 +0800

    Feature: master Subject: 冒泡排序优化 Reviewer: Leach

--- leach/src/main/java/sortAlgoeithm/BubbleSorting.java
+++ leach/src/main/java/sortAlgoeithm/BubbleSorting.java
@@ -9,7 +9,7 @@ public class BubbleSorting {
     public static void bubbleSorting(int[] arr){
         int size = arr.length-1;
         for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size ; j++) {
+            for (int j = 0; j < size - i; j++) {
                 if (arr[j]>arr[j+1]){
                     SortUtils.swap(arr,j,j+1);
                 }
