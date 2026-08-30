2326d4c1b7c977d416223324e074c2443f48fdc8
vission <vission8899@gmail.com>
2026-08-30 18:13:30 +0800

    P1,两数之和O(n)

--- src/main/java/top/vission/problems/impl/P1.java
+++ src/main/java/top/vission/problems/impl/P1.java
@@ -3,13 +3,15 @@ package top.vission.problems.impl;
 import lombok.extern.slf4j.Slf4j;
 import top.vission.problems.LeetCodeProblemRun;
 
+import java.util.HashMap;
+
 @Slf4j
 public class P1 implements LeetCodeProblemRun {
 
     @Override
     public void run() {
         int[] nums = {1, 2, 5};
-        twoSum(nums, 3);
+        twoSum2(nums, 3);
     }
 
     public int[] twoSum(int[] nums, int target) {
@@ -22,6 +24,22 @@ public class P1 implements LeetCodeProblemRun {
                     result[1] = j;
                 }
             }
+        }
+        log.info("结果:{}", result);
+        return result;
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        log.info("TestCase:{},target:{}", nums, target);
+        int[] result = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int key = target - nums[i];
+            if (map.containsKey(key)) {
+                result[1] = map.get(key);
+                result[0] = i;
+            }
+            map.put(nums[i],i);
         }
         log.info("结果:{}", result);
         return result;
